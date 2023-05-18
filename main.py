ba9dac31aa870f0ec216be512d28bcefca64bb09
Pavel <borisov.p.92@yandex.ru>
2023-05-18 12:51:03 +0300

    Final version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import csv
 
 
 RESULT_FOLDER = 'data'
@@ -18,12 +19,24 @@ EXCEPT_CATEGORIES = [
 ]
 
 
+def data_to_csv(data: str):
+    PATH_TO_RESULT_FILE = os.path.join(PATH_TO_RESULT, RESULT_FILE_NAME)
+
+    if not os.path.exists(PATH_TO_RESULT_FILE):
+        with open(PATH_TO_RESULT_FILE, 'w', encoding='utf-8', newline='') as csv_file:
+            csvwriter = csv.writer(csv_file)
+            csvwriter.writerow([data])
+
+    else:
+        with open(PATH_TO_RESULT_FILE, 'a', encoding='utf-8', newline='') as csv_file:
+            csvwriter = csv.writer(csv_file)
+            csvwriter.writerow([data])
+
+
 def get_data(url: str):
     headers = {
-        'accept': '*/*',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'ru,en;q=0.9,de;q=0.8',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.4.603 Yowser/2.5 Safari/537.36'
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
     }
 
     req = requests.get(url, headers=headers)
@@ -37,14 +50,73 @@ def get_data(url: str):
     bs = BeautifulSoup(html_code, 'lxml')
     category_links = bs.find(class_='single').find_all('a')
 
-    category_page_links = []
+    category_page_links = {}
 
     for category in category_links:
         if category.text not in EXCEPT_CATEGORIES:
             link = 'https://souo-mos.ru' + category['href']
-            category_page_links.append(link)
+            category_page_links[category.text] = link
 
-    print(category_page_links)
+    count = 0
+
+    for category, link in category_page_links.items():
+        print(f'\nВыполняется обработка в категории {category}...')
+        
+        req = requests.get(link, headers=headers)
+
+        with open(f'{HTML_PAGES_PATH}/{category}.html', 'w', encoding='utf-8') as file:
+            file.write(req.text)
+
+        with open(f'{HTML_PAGES_PATH}/{category}.html', 'r', encoding='utf-8') as html_file:
+            html_code = html_file.read()
+
+        bs = BeautifulSoup(html_code, 'lxml')
+        boxes = bs.find(class_='posts').find_all(class_='box')
+        
+        if not boxes:
+            print('Данных в данной категории нет...\n')
+            count += 1
+
+            continue
+
+        total_pages = int(bs.find('a', attrs={'class': 'last'})['href'].split('/')[-2])
+        
+        for page in range(1, total_pages + 1):
+            print(f'\nСтраница {page}')
+
+            page_link = link + f'page/{page}/'
+            req = requests.get(page_link, headers=headers)
+            
+            with open(f'{HTML_PAGES_PATH}/{category}_{page}.html', 'w', encoding='utf-8') as file:
+                file.write(req.text)
+
+            with open(f'{HTML_PAGES_PATH}/{category}_{page}.html', 'r', encoding='utf-8') as file:
+                html = file.read()
+
+            bs = BeautifulSoup(html, 'lxml')
+
+            boxes = bs.find('div', attrs={'id': 'boxes'}).find_all(class_='box')
+
+            for box in boxes:
+                if box.find('a').text.lower() != 'тестовая организация':
+                    name_firm = box.find('a').text
+                    firm_link = box.find('a')['href']
+
+                    req = requests.get(firm_link, headers=headers)
+                    bs = BeautifulSoup(req.text, 'lxml')
+
+                    try:
+                        site = bs.find(class_='posts').find('article').find('a', attrs={'itemprop': 'url'})['href']
+
+                    except TypeError:
+                        print(f'Сайт фирмы {name_firm} не указан')
+                        continue
+
+                    data_to_csv(site)
+
+                    print(f'Сайт фирмы {name_firm} записан...')
+                        
+        count += 1            
 
 
 def main():
@@ -55,6 +127,8 @@ def main():
         os.mkdir(HTML_PAGES_PATH)
 
     get_data('https://souo-mos.ru/spravochnyk/avto/')
+
+    print('\nРабота завершена.')
 
 
 if __name__ == '__main__':
